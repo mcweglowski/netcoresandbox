47968c2515267e3452a6decb1bdaec7875a6fcb7
Marcin Węglowski <mcweglowski@gmail.com>
2021-03-05 15:55:12 +0100

    Datbase model

---
+++ RestaurantAPI/Entites/RestaurantDbContext.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantAPI.Entities
+{
+    public class RestaurantDbContext : DbContext
+    {
+        private string _connectionString = 
+            "Server=.;Database=RestaurantDb;Trusted_Connection=True;";
+
+         public DbSet<Restaurant> Restaurants { get; set; }
+         public DbSet<Address> Addresses { get; set; }
+         public DbSet<Dish> Dishes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Restaurant>()
+                .Property(r => r.Name)
+                .IsRequired()
+                .HasMaxLength(25);
+
+            modelBuilder.Entity<Dish>()
+                .Property(d => d.Name)
+                .IsRequired();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuiler)
+        {
+             optionsBuiler.UseSqlServer(_connectionString);
+        }
+    }
+}
